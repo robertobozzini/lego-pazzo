68a8bfd45faa32bb52778f2e504b185f47fa009d
IlTia21 <mttiaver21@gmail.com>
2025-06-09 16:34:23 +0200

    test+

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Button, Color, ImageFile, SoundFile
 from pybricks.tools import wait
 
-
 ev3 = EV3Brick()
 
 color_sensor = ColorSensor(Port.S3)
+color_sensor_old = color_sensor.color()
 
 def send_update(color):
     if color == Color.RED:
-        # chiama IoT e fa 
+        print(color)
+        # Chiama IoT e fa
     elif color == Color.BLUE:
-        # chiama IoT e fa
+        print(color)
+        # Chiama IoT e fa
+        
+def main():
+    while True:
+        if color_sensor.color() != color_sensor_old: # quando cambia colore chiama AWS
+            color_sensor_old = color_sensor.color()
+            send_update(color_sensor.color())
