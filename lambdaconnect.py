5b3ec8653c13379ce995ba3924e3dead3169aca6
roberto.bozzini <robbozz2007@gmail.com>
2025-06-10 18:07:58 +0200

    lambdaconnect_inizio

---
+++ lambdaconnect.py
@@ -0,0 +1,15 @@
+
+import boto3
+import json
+ddb=boto3.resource('dynamodb',region_name='eu-north-1')
+# table=dynamodb.Table('Alternanza_CLI')
+table = ddb.Table('lego-pazzo')
+def lambda_handler(event, context):
+    instruction=event['requestContext']['inst'] 
+    connection_id = event['requestContext']['connectionId']
+    # ddb = boto3.resource('dynamodb')
+    if(instruction==0):
+        table.put_item(Item={'PK':'connection','SK':connection_id,'stato':1})
+    else:
+        table.delete_item(Key={'PK':"connection",'SK':connection_id})
+    return {'statusCode': 200}
