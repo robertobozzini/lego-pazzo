3242464277c282391e956bae5602f94858c032f9
IlTia21 <mttiaver21@gmail.com>
2025-06-10 12:42:14 +0200

    Added topics

--- IoT_integration.py
+++ IoT_integration.py
@@ -7,7 +7,8 @@ endpoint = "your-iot-endpoint.amazonaws.com"
 root_ca = "AmazonRootCA1.pem"
 private_key = "private.key"
 certificate = "certificate.pem.crt"
-topic = "test/topic"
+topic_S = "$aws/things/Centralina/shadow/update/documents"
+topic_R = "topic3"
 
 client = AWSIoTMQTTClient(client_id)
 client.configureEndpoint(endpoint, 8883)
@@ -17,13 +18,13 @@ client.connect()
 
 def send_update(color):
     if color == Color.RED:
-        client.publish(topic, json.dumps({"id" : "red_car", "status" : "in"}), 1)
+        client.publish(topic_S, json.dumps({"id" : "red_car", "status" : "in"}), 1)
     elif color == Color.BLUE:
-        client.publish(topic, json.dumps({"id" : "blue_car", "status" : "in"}), 1)
+        client.publish(topic_S, json.dumps({"id" : "blue_car", "status" : "in"}), 1)
     elif color_sensor_old == Color.RED:
-        client.publish(topic, json.dumps({"id" : "red_car", "status" : "out"}), 1) 
+        client.publish(topic_S, json.dumps({"id" : "red_car", "status" : "out"}), 1) 
     elif color_sensor_old == Color.BLUE:
-        client.publish(topic, json.dumps({"id" : "blue_car", "status" : "out"}), 1) 
+        client.publish(topic_S, json.dumps({"id" : "blue_car", "status" : "out"}), 1) 
     color_sensor_old = color
     
 def modify_status(id, status): # applica le modifiche
@@ -36,10 +37,7 @@ def apply_update(client, userdata, message): # riceve update mandati da IoT
     for id, status in data.items():
         modify_status(id, status)
         
-client.subscribe(topic, 1, apply_update)
-
-
-
+client.subscribe(topic_R, 1, apply_update)
 
 #     Turn on your device and make sure it's connected to the internet.
 #     Choose how you want to load files onto your device.
@@ -53,3 +51,7 @@ client.subscribe(topic, 1, apply_update)
 #     ping a2dvm6ijdlolkr-ats.iot.eu-north-1.amazonaws.com
 
 # After you complete these steps and get a successful ping response, you're ready to continue and connect your device to AWS IoT.
+
+# unzip connect_device_package.zip
+# chmod +x start.sh
+# ./start.sh
