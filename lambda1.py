0145575eb6e06298aa374bc72ba34ea139ba3f36
Mario Agati <agati.mario.93+github@gmail.com>
2025-06-11 11:58:23 +0200

    fix: fixed lambda1 to save data on dynamodb

--- lambda1.py
+++ lambda1.py
@@ -1,15 +1,15 @@
 import json
 import boto3
+from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb', region_name='eu-north-1')
 table = dynamodb.Table('lego-pazzo2')
 
 def get_all_connection_ids():
-    response = table.scan(
-        FilterExpression="begins_with(SK, :prefix)",
-        ExpressionAttributeValues={":prefix": "conn#"}
+    response = table.query(
+        KeyConditionExpression=Key('PK').eq('connection'),
     )
-    return [item['SK'].replace("conn#", "") for item in response['Items']]
+    return [item['SK'] for item in response['Items']]
 
 def send_to_all_connections(data, endpoint_url):
     client = boto3.client(
@@ -31,46 +31,68 @@ def send_to_all_connections(data, endpoint_url):
 
 def lambda_handler(event, context):
     print("Evento ricevuto:", event)
-    
     message = event.get('payload') or event.get('message') or event
     if isinstance(message, str):
         try:
             message = json.loads(message)
         except json.JSONDecodeError:
             print("Errore nel parsing del JSON")
+            return
 
     print("Messaggio dopo parsing:", message)
-    device = message.get('SK')
-    print("Dispositivo:", device)
+    client = message.get('state', {}).get("reported", {}).get('device', {}).get('client')
+    print("Client", client)
+    if not client or client == 'aws':
+        print("Client aws, ignoro il messaggio.")
+        return
 
-    if device in ("red_car", "blue_car"):
-        id_val_car = device
-        car_status = message['stato']
+    ligth_sensor = message.get('state', {}).get("reported", {}).get('sensors', {}).get('light')
+    motor = message.get('state', {}).get("reported", {}).get('sensors', {}).get('motor')
 
-        verifica = table.get_item(Key={'SK': id_val_car, 'PK': 'sensori'})
-        item = verifica.get('Item')
+    table.put_item(
+        Item={
+            'PK': 'sensori',
+            'SK': 'light_sensor',
+            'valore': ligth_sensor
+        }
+    )
 
-        if not item or item.get('stato') != car_status:
-            table.update_item(
-                Key={'SK': id_val_car, 'PK': 'sensori'},
-                UpdateExpression="SET stato = :s",
-                ExpressionAttributeValues={":s": car_status}
-            )
+    table.put_item(
+        Item={
+            'PK': 'sensori',
+            'SK': 'motor',
+            'valore': motor
+        }
+    )
 
-            updated_item = {"SK": id_val_car, "stato": car_status}
+    # if device in ("red_car", "blue_car"):
+    #     id_val_car = device
+    #     car_status = message['stato']
 
-            gate_value = "90" if car_status == "in" else "0"
-            table.update_item(
-                Key={'SK': "gate_motor", 'PK': 'sensori'},
-                UpdateExpression="SET stato = :s",
-                ExpressionAttributeValues={":s": gate_value}
-            )
+    #     verifica = table.get_item(Key={'SK': id_val_car, 'PK': 'sensori'})
+    #     item = verifica.get('Item')
 
-            updated_item_gate = {"SK": "gate_motor", "stato": gate_value}
+    #     if not item or item.get('stato') != car_status:
+    #         table.update_item(
+    #             Key={'SK': id_val_car, 'PK': 'sensori'},
+    #             UpdateExpression="SET stato = :s",
+    #             ExpressionAttributeValues={":s": car_status}
+    #         )
 
-            endpoint = "wss://5l3ibg8vdb.execute-api.eu-north-1.amazonaws.com"  # <-- Metti il tuo endpoint WebSocket
+    #         updated_item = {"SK": id_val_car, "stato": car_status}
 
-            send_to_all_connections([updated_item, updated_item_gate], endpoint)
+    #         gate_value = "90" if car_status == "in" else "0"
+    #         table.update_item(
+    #             Key={'SK': "gate_motor", 'PK': 'sensori'},
+    #             UpdateExpression="SET stato = :s",
+    #             ExpressionAttributeValues={":s": gate_value}
+    #         )
+
+    #         updated_item_gate = {"SK": "gate_motor", "stato": gate_value}
+
+    #         endpoint = "wss://5l3ibg8vdb.execute-api.eu-north-1.amazonaws.com"  # <-- Metti il tuo endpoint WebSocket
+
+    #         send_to_all_connections([updated_item, updated_item_gate], endpoint)
 
     return {
         "statusCode": 200,
