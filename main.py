165675e2f125d5cc767175c33739b3fde9e33457
IlTia21 <mttiaver21@gmail.com>
2025-06-11 10:41:14 +0200

    Working 2.0

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ def main():
     while True:
         if color_sensor.color() != color_sensor_old: # quando cambia colore chiama AWS
             send_update(color_sensor.color())
+            
+# tutto ok tranne lambda 2/3
